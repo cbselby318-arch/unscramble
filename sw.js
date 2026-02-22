baf19b9d8e40ca0f2fb61b8a602c66dbd21b8c7c
cbselby318-arch <cbselby318-arch@users.noreply.github.com>
2026-02-22 15:12:33 -0700

    show flag + state only, drop redundant country code

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-﻿const CACHE_NAME = 'unscramble-v2';
+﻿const CACHE_NAME = 'unscramble-v3';
 const ASSETS = ['index.html', 'manifest.json'];
 
 self.addEventListener('install', (e) => {
@@ -31,4 +31,5 @@ self.addEventListener('fetch', (e) => {
     })
   );
 });
+
 
